4cd261653bc7b3fb60ec710a3a586a92729d0b44
GauravPrakashh <129797286+iamgaurav12@users.noreply.github.com>
2025-02-14 00:18:11 +0530

    Update referral.route.ts

--- src/routes/referral.route.ts
+++ src/routes/referral.route.ts
@@ -3,7 +3,8 @@ import { createReferral } from "../controllers/referral.controller";
 const router = express.Router();
 
 router.post("/create", createReferral, (req, res, next) => {
-    next();
+    res.status(200).send("Referral created successfully!");
 });
+
 
 export default router;
